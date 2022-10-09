66fdccae935acecf14b577442120f466351f2cfa
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-10-10 04:13:41 +0600

    some changed

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -83,7 +83,7 @@ const Navbar = () => {
                 <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
               </svg>
             </a>
-            <ul className="p-2">
+            <ul className="p-2 text-white">
               <li>
                 <a>Logo Design</a>
               </li>
