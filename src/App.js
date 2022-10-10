4c984a34b857e062c7c3793c025c1379a8defb4d
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-10-10 14:55:37 +0600

    tasting whatsapp

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./Shared/Navbar/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home/Home";
 import Footer from "./Shared/Footer/Footer";
-import Login from './Pages/Home/Login/Login.jsx'
+import Login from "./Pages/Home/Login/Login.jsx";
 
 function App() {
   return (
