12db68e892116883dfca763e58c0306fe70ba44d
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-10-12 12:22:12 +0600

    some changed

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -66,7 +66,7 @@ const Services = () => {
       <div className="grid grid-cols-1 lg:grid-cols-4 my-10 gap-10 mx-5 lg:mx-40">
         {data.map((d) => (
           <Link to={d.route}>
-            <div className="card h-72 glass flex justify-center items-center hover:bg-blue-600 transition ease-in duration-300 hover:text-white cursor-pointer pb-5">
+            <div className="card h-72 glass flex justify-center items-center lg:bg-white text-white lg:text-black bg-blue-600 hover:bg-blue-600 transition ease-in duration-300 hover:text-white cursor-pointer pb-5">
               <figure>
                 <span className="pt-10">{d.icon}</span>
               </figure>
