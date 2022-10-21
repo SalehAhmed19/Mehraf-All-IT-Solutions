23bc817228dc1a31421eab37e2c34bc5bf6c55a0
Saleh Ahmed Mahin <salehahmedmahin@Salehs-MacBook-Air.local>
2022-10-21 15:35:42 +0600

    services responsive for mid device

--- src/Pages/Home/Services/Services.jsx
+++ src/Pages/Home/Services/Services.jsx
@@ -56,7 +56,7 @@ const Services = () => {
           className="h-1 w-36 rounded"
         ></div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-4 my-10 gap-10 mx-5 lg:mx-40">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 my-10 gap-10 mx-5 lg:mx-40">
         {data.map((d) => (
           <Link to={d.route}>
             <div className="card h-72 glass flex justify-center items-center lg:bg-white text-white lg:text-black bg-blue-600 hover:bg-blue-600 transition ease-in duration-300 hover:text-white cursor-pointer pb-5">
