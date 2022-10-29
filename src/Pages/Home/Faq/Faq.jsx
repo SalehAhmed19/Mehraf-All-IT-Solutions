fb8ffa50ed100d0984d384e7ed7714541b8115b8
Rashidaakter1 <rashidaakterchadni@gmail.com>
2022-10-29 21:39:02 +0600

    fix the problem in faq section

--- src/Pages/Home/Faq/Faq.jsx
+++ src/Pages/Home/Faq/Faq.jsx
@@ -18,7 +18,7 @@ export default function Faq() {
 
   return (
     <div id="faq" className="my-28">
-      <Box sx={{ margin: "3rem", marginTop: "7rem" }}>
+      <Box className="lg:ml-28  lg:mr-28 ml-6 mr-6" >
         <Fade left>
           <Typography
             variant="h4"
@@ -34,7 +34,7 @@ export default function Faq() {
           </Typography>
         </Fade>
         <Fade right>
-          <Box sx={{ marginLeft: "9rem", marginRight: "9rem" }}>
+          <Box >
             {" "}
             <Accordion
               sx={{ padding: "1.5rem" }}
@@ -164,5 +164,95 @@ export default function Faq() {
         </Fade>
       </Box>
     </div>
+    // <div>
+    //   <Accordion
+    //     expanded={expanded === "panel1"}
+    //     onChange={handleChange("panel1")}
+    //   >
+    //     <AccordionSummary
+    //       expandIcon={<ExpandMore />}
+    //       aria-controls="panel1bh-content"
+    //       id="panel1bh-header"
+    //     >
+    //       <Typography sx={{ width: "100%", flexShrink: 0 }}>
+    //         General settings
+    //       </Typography>
+    //       <Typography sx={{ color: "text.secondary" }}>
+    //         I am an accordion
+    //       </Typography>
+    //     </AccordionSummary>
+    //     <AccordionDetails>
+    //       <Typography>
+    //         Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat.
+    //         Aliquam eget maximus est, id dignissim quam.
+    //       </Typography>
+    //     </AccordionDetails>
+    //   </Accordion>
+    //   <Accordion
+    //     expanded={expanded === "panel2"}
+    //     onChange={handleChange("panel2")}
+    //   >
+    //     <AccordionSummary
+    //       expandIcon={<ExpandMore />}
+    //       aria-controls="panel2bh-content"
+    //       id="panel2bh-header"
+    //     >
+    //       <Typography sx={{ width: "33%", flexShrink: 0 }}>Users</Typography>
+    //       <Typography sx={{ color: "text.secondary" }}>
+    //         You are currently not an owner
+    //       </Typography>
+    //     </AccordionSummary>
+    //     <AccordionDetails>
+    //       <Typography>
+    //         Donec placerat, lectus sed mattis semper, neque lectus feugiat
+    //         lectus, varius pulvinar diam eros in elit. Pellentesque convallis
+    //         laoreet laoreet.
+    //       </Typography>
+    //     </AccordionDetails>
+    //   </Accordion>
+    //   <Accordion
+    //     expanded={expanded === "panel3"}
+    //     onChange={handleChange("panel3")}
+    //   >
+    //     <AccordionSummary
+    //       expandIcon={<ExpandMore />}
+    //       aria-controls="panel3bh-content"
+    //       id="panel3bh-header"
+    //     >
+    //       <Typography sx={{ width: "33%", flexShrink: 0 }}>
+    //         Advanced settings
+    //       </Typography>
+    //       <Typography sx={{ color: "text.secondary" }}>
+    //         Filtering has been entirely disabled for whole web server
+    //       </Typography>
+    //     </AccordionSummary>
+    //     <AccordionDetails>
+    //       <Typography>
+    //         Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
+    //         sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+    //       </Typography>
+    //     </AccordionDetails>
+    //   </Accordion>
+    //   <Accordion
+    //     expanded={expanded === "panel4"}
+    //     onChange={handleChange("panel4")}
+    //   >
+    //     <AccordionSummary
+    //       expandIcon={<ExpandMore />}
+    //       aria-controls="panel4bh-content"
+    //       id="panel4bh-header"
+    //     >
+    //       <Typography sx={{ width: "33%", flexShrink: 0 }}>
+    //         Personal data
+    //       </Typography>
+    //     </AccordionSummary>
+    //     <AccordionDetails>
+    //       <Typography>
+    //         Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer
+    //         sit amet egestas eros, vitae egestas augue. Duis vel est augue.
+    //       </Typography>
+    //     </AccordionDetails>
+    //   </Accordion>
+    // </div>
   );
 }
